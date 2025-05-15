6996393a80e3fa83dcfd7de99b2d991b31695782
José Mauro <josemaurosilva600@gmail.com>
2025-05-15 12:31:00 -0300

    Primeiro commit

---
+++ src/test/java/com/senai/josemauro/csi_apialugueldeveiculos/CsiApiAluguelDeVeiculosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.senai.josemauro.csi_apialugueldeveiculos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsiApiAluguelDeVeiculosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
